b64363c33b0d687452f18612ddda30ef884c67b4
Mikel Etxarri <mikel.echarri@alumni.mondragon.edu>
2025-11-28 09:01:41 +0100

    Backend: Springboot project for OS

---
+++ backend/simulator-os/src/main/java/com/shelterai/simulator_os/SimulatorOsApplication.java
@@ -0,0 +1,13 @@
+package com.shelterai.simulator_os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorOsApplication.class, args);
+	}
+
+}
---
+++ backend/simulator-os/src/test/java/com/shelterai/simulator_os/SimulatorOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelterai.simulator_os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
